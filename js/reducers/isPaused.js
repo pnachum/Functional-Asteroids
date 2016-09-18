37335f32787ac0ad098203358fd920abd0f5a048
Phil Nachum <phil.nachum@airbnb.com>
2016-09-18 13:40:41 -0700

    remove comment

--- js/reducers/isPaused.js
+++ js/reducers/isPaused.js
@@ -2,7 +2,6 @@
 
 import { TOGGLE_PAUSE } from '../actionCreators';
 
-// state is a boolean
 export default function isPaused(state: boolean = false, action: Object): boolean {
   switch (action.type) {
     case TOGGLE_PAUSE:
