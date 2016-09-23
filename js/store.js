a92c242f8c569b4ef0773c043ece17576ea4ade5
Phil Nachum <phil.nachum@airbnb.com>
2016-09-23 08:10:52 -0700

    Remove immutability check

--- js/store.js
+++ js/store.js
@@ -1,11 +1,9 @@
 // @flow
 
 import { createStore, applyMiddleware } from 'redux';
-import immutableCheckMiddleWare from 'redux-immutable-state-invariant';
 import rootReducer from './reducers/root';
 
 const middleWare = [];
-middleWare.push(immutableCheckMiddleWare());
 
 const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
 
